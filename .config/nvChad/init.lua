a0b3b3f8d65611c87285d665625d6e15264e84db
Ye Yint Aung <yya@za.com.mm>
2025-07-21 16:21:03 +0630

    mod nv chad

--- .config/nvChad/init.lua
+++ .config/nvChad/init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
+vim.o.relativenumber = true
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
--- .config/nvChad/lua/chadrc.lua
+++ .config/nvChad/lua/chadrc.lua
@@ -1,24 +1,21 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
----@type ChadrcConfig
-local M = {}
-
-M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+return {
+  base46 = {
+    theme = "onedark",
+  },
+  nvdash = { load_on_startup = true },
+  ui = {
+    statusline = {
+      enabled = true,
+      theme = "default", -- default/vscode/vscode_colored/minimal
+      -- default/round/block/arrow separators work only for default statusline theme
+      -- round and block will work for minimal theme only
+      separator_style = "round",
+      order = nil,
+      modules = nil,
+    },
+  },
 }
-
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
-
-return M
