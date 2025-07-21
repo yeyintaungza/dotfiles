2e061e0901823b237160747fefe0230020894749
Ye Yint Aung <yya@za.com.mm>
2025-07-21 15:16:28 +0630

    nv chad config

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,28 +1,9 @@
 return {
-    {
-        "sainnhe/gruvbox-material",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.gruvbox_material_enable_italic = true
-            vim.g.gruvbox_material_background = "medium"
-            vim.g.gruvbox_material_foreground = "original"
-        end,
-    },
-    {
-        "navarasu/onedark.nvim",
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require("onedark").setup({
-                style = "darker",
-            })
-        end,
-    },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "onedark",
+            colorscheme = "tokyonight-night",
         },
     },
 }
