90d46df91c0d844bfc2693b89474ffcfbd63e41c
Ye Yint Aung <yya@za.com.mm>
2025-07-22 15:19:15 +0630

    *

--- .config/nvChad/init.lua
+++ .config/nvChad/init.lua
@@ -30,9 +30,6 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "options"
-require "autocmds"
-
 vim.schedule(function()
   require "mappings"
 end)
---
+++ .config/nvChad/lua/plugins/chadrc.lua
@@ -0,0 +1,21 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+
+return {
+  base46 = {
+    theme = "bearded-arc",
+  },
+  nvdash = { load_on_startup = true },
+  ui = {
+    statusline = {
+      enabled = true,
+      theme = "default", -- default/vscode/vscode_colored/minimal
+      -- default/round/block/arrow separators work only for default statusline theme
+      -- round and block will work for minimal theme only
+      separator_style = "round",
+      order = nil,
+      modules = nil,
+    },
+  },
+}
