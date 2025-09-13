a2a6d68cba2327ceb8281a4c529188dbb0f0e16c
Ye Yint Aung <yya@za.com.mm>
2025-09-13 16:21:57 +0630

    idea vim rc

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,9 +1,16 @@
 return {
 
     {
+        "olimorris/onedarkpro.nvim",
+        priority = 1000, -- Ensure it loads first
+    },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+    { "Shatur/neovim-ayu" },
+
+    {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "tokyonight-night",
+            colorscheme = "gruvbox",
         },
     },
 }
---
+++ .config/nvim/lua/plugins/flutter_bloc.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "wa11breaker/flutter-bloc.nvim",
+        dependencies = {
+            "nvimtools/none-ls.nvim", -- Required for code actions
+        },
+        opts = {
+            bloc_type = "default", -- Choose from: 'default', 'equatable', 'freezed'
+            use_sealed_classes = false,
+            enable_code_actions = true,
+        },
+    },
+}
