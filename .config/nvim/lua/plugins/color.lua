66772a7a0a5e35a52594376d78b8989ad0ab7c10
Ye Yint Aung <yya@za.com.mm>
2025-12-01 09:17:52 +0630

    transparent in github theme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,6 +1,16 @@
 return {
 
-  { "projekt0n/github-nvim-theme", name = "github-theme" },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
   {
     "navarasu/onedark.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
@@ -8,6 +18,16 @@ return {
       require("onedark").setup({
         style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
         transparent = true,
+      })
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = ...,
+    config = function()
+      require("gruvbox").setup({
+        --
       })
     end,
   },
