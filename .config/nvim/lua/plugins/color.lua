9eba00e55eace313ab9b14244abd7ebdc6434dae
Ye Yint Aung <yya@za.com.mm>
2025-12-03 14:35:00 +0630

    some config in ideavim rc

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -12,26 +12,6 @@ return {
     end,
   },
   {
-    "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("onedark").setup({
-        style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-        transparent = true,
-      })
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    opts = ...,
-    config = function()
-      require("gruvbox").setup({
-        --
-      })
-    end,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "github_dark_dimmed",
