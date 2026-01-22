ff8e094cf9a24b9affcdd04514b74d2e9e3bfe95
Ye Yint Aung <yya@za.com.mm>
2026-01-22 09:10:30 +0630

    line height in vs code settings.json

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,10 +1,41 @@
 return {
-  { "Shatur/neovim-ayu" },
+  {
+    "Shatur/neovim-ayu",
+    config = function()
+      require("ayu").setup({
+        overrides = {
+          Normal = { bg = "None" },
+          NormalFloat = { bg = "none" },
+          ColorColumn = { bg = "None" },
+          SignColumn = { bg = "None" },
+          Folded = { bg = "None" },
+          FoldColumn = { bg = "None" },
+          CursorLine = { bg = "None" },
+          CursorColumn = { bg = "None" },
+          VertSplit = { bg = "None" },
+        },
+      })
+    end,
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "ayu-dark",
+      colorscheme = "github_dark_dimmed",
     },
   },
 }
