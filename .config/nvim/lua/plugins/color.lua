4de6043ca5499b4e40f0d3648c6174ff4a5638ef
Ye Yint Aung <yya@za.com.mm>
2026-04-02 15:34:47 +0630

    helix config update

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,20 +1,10 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-    config = function()
-      require("onedark").setup({
-        style = "deep",
-        transparent = false,
-      })
-    end,
-  },
 
   {
     "rebelot/kanagawa.nvim",
     config = function()
       require("kanagawa").setup({
-        transparent = true,
+        --transparent = true,
         colors = {
           theme = {
             all = {
@@ -24,25 +14,25 @@ return {
             },
           },
         },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          }
-        end,
+        -- overrides = function(colors)
+        --   local theme = colors.theme
+        --   return {
+        --     TelescopeTitle = { fg = theme.ui.special, bold = true },
+        --     TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        --     TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        --     TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        --     TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        --     TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        --     TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+        --
+        --     NormalFloat = { bg = "none" },
+        --     FloatBorder = { bg = "none" },
+        --     FloatTitle = { bg = "none" },
+        --     NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        --     LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        --     MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        --   }
+        -- end,
       })
     end,
   },
