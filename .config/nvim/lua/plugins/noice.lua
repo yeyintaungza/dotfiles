1dd9fc8047c1c34d49ccfba43f27ce3aaa933d56
Ye Yint Aung <yya@za.com.mm>
2026-06-30 16:29:44 +0630

    lsp border

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    presets = {
+      lsp_doc_border = true,
+    },
+    views = {
+      hover = {
+        border = {
+          style = "rounded", -- single, double, rounded, solid, shadow
+        },
+      },
+    },
+  },
+}
