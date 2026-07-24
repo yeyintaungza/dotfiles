227eefb21c0008dc0d8fcbab578c441f9d16b050
Ye Yint Aung <yya@za.com.mm>
2026-07-24 11:40:35 +0630

    tokyonight

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -28,11 +28,102 @@ return {
       })
     end,
   },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+    end,
+  },
+  { "yorumicolors/yorumi.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      local transparent = false
+
+      local coolnight = {
+        bg = "#011323",
+        bg_dark = "#00111E",
+        bg_float = "#00111E",
+        bg_highlight = "#012646",
+        bg_search = "#3E90D7",
+        bg_sidebar = "#00111E",
+        bg_statusline = "#00111E",
+        bg_visual = "#064984",
+
+        fg = "#E3EEF7",
+        fg_dark = "#AECBE5",
+        fg_float = "#CBDFF0",
+        fg_gutter = "#2D4F6C",
+        fg_sidebar = "#AECBE5",
+
+        border = "#03447C",
+      }
+
+      require("tokyonight").setup({
+        style = "storm",
+        transparent = transparent,
+        lualine_bold = true,
+
+        styles = {
+          sidebars = transparent and "transparent" or "dark",
+          floats = transparent and "transparent" or "dark",
+        },
+
+        on_colors = function(c)
+          c.bg = coolnight.bg
+          c.bg_dark = transparent and c.none or coolnight.bg_dark
+          c.bg_float = transparent and c.none or coolnight.bg_float
+          c.bg_sidebar = transparent and c.none or coolnight.bg_sidebar
+          c.bg_statusline = transparent and c.none or coolnight.bg_statusline
+          c.bg_highlight = coolnight.bg_highlight
+          c.bg_visual = coolnight.bg_visual
+          c.bg_search = coolnight.bg_search
+
+          c.fg = coolnight.fg
+          c.fg_dark = coolnight.fg_dark
+          c.fg_float = coolnight.fg_float
+          c.fg_sidebar = coolnight.fg_sidebar
+          c.fg_gutter = coolnight.fg_gutter
+        end,
+
+        on_highlights = function(hl, c)
+          hl.FloatBorder = {
+            fg = coolnight.border,
+            bg = transparent and "NONE" or c.bg_float,
+          }
+
+          hl.NormalFloat = {
+            bg = transparent and "NONE" or c.bg_float,
+          }
+
+          hl.WinSeparator = {
+            fg = coolnight.border,
+          }
+
+          hl.VertSplit = {
+            fg = coolnight.border,
+          }
+
+          hl.Search = {
+            bg = coolnight.bg_search,
+            fg = c.bg,
+          }
+
+          hl.Visual = {
+            bg = coolnight.bg_visual,
+          }
+        end,
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "yorumi",
     },
   },
 }
