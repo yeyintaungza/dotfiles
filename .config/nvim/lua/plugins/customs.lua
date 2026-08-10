104c68f39b0f0ea8696caea589cf4fd6b823cdc2
Ye Yint Aung <yya@za.com.mm>
2026-08-10 13:20:07 +0630

    nvim color update

--- .config/nvim/lua/plugins/customs.lua
+++ .config/nvim/lua/plugins/customs.lua
@@ -1,4 +1,222 @@
 return {
+  { "nvim-mini/mini.icons", enable = false },
+  { "nvim-tree/nvim-web-devicons" },
+  { "kdheepak/lazygit.nvim", enable = false },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = false,
+
+      on_colors = function(colors)
+        colors.bg = "#0B1220"
+        colors.bg_dark = "#080F1C"
+        colors.bg_sidebar = "#080F1C"
+        colors.bg_float = "#101B2D"
+        colors.bg_highlight = "#1A304B"
+
+        colors.blue = "#82AAFF"
+        colors.cyan = "#7FDBCA"
+        colors.green = "#22DA6E"
+        colors.orange = "#F78C6C"
+        colors.yellow = "#ECC48D"
+        colors.red = "#EF5350"
+        colors.magenta = "#C792EA"
+
+        colors.fg = "#C4D2E5"
+        colors.fg_dark = "#8296B2"
+        colors.comment = "#5C6D84"
+
+        colors.border = "#263A54"
+        colors.fg_gutter = "#34445B"
+      end,
+
+      on_highlights = function(hl, c)
+        hl.Normal = {
+          fg = c.fg,
+          bg = c.bg,
+        }
+
+        hl.NormalNC = {
+          fg = c.fg,
+          bg = c.bg,
+        }
+
+        hl.NormalFloat = {
+          fg = c.fg,
+          bg = c.bg_float,
+        }
+
+        hl.FloatBorder = {
+          fg = "#263A54",
+          bg = c.bg_float,
+        }
+
+        hl.FloatTitle = {
+          fg = c.blue,
+          bg = c.bg_float,
+          bold = true,
+        }
+
+        hl.CursorLine = {
+          bg = "#101B2D",
+        }
+
+        hl.CursorLineNr = {
+          fg = "#8296B2",
+          bold = true,
+        }
+
+        hl.Visual = {
+          bg = "#1A304B",
+        }
+
+        hl.Search = {
+          fg = c.fg,
+          bg = "#1C3552",
+        }
+
+        hl.IncSearch = {
+          fg = "#0B1220",
+          bg = "#ECC48D",
+        }
+
+        hl.CurSearch = {
+          fg = "#0B1220",
+          bg = "#ECC48D",
+        }
+
+        hl.LineNr = {
+          fg = "#34445B",
+        }
+
+        hl.SignColumn = {
+          bg = "#080F1C",
+        }
+
+        hl.GitSignsAdd = {
+          fg = "#294D45",
+          bg = "#080F1C",
+        }
+
+        hl.GitSignsChange = {
+          fg = "#31547A",
+          bg = "#080F1C",
+        }
+
+        hl.GitSignsDelete = {
+          fg = "#5A3440",
+          bg = "#080F1C",
+        }
+
+        hl.Pmenu = {
+          fg = c.fg,
+          bg = "#101B2D",
+        }
+
+        hl.PmenuSel = {
+          fg = c.fg,
+          bg = "#1A304B",
+        }
+
+        hl.PmenuSbar = {
+          bg = "#1A2B42",
+        }
+
+        hl.PmenuThumb = {
+          bg = "#304A6D",
+        }
+
+        hl.LspInfoBorder = {
+          fg = "#263A54",
+          bg = "#101B2D",
+        }
+
+        hl.HoverNormal = {
+          fg = c.fg,
+          bg = "#101B2D",
+        }
+
+        hl.MsgArea = {
+          fg = c.fg,
+          bg = c.bg,
+        }
+
+        hl.MoreMsg = {
+          fg = c.blue,
+        }
+
+        hl.Question = {
+          fg = c.cyan,
+        }
+
+        hl.ErrorMsg = {
+          fg = c.red,
+        }
+
+        hl.WarningMsg = {
+          fg = c.yellow,
+        }
+
+        hl.StatusLine = {
+          fg = c.fg,
+          bg = "#0D1728",
+        }
+
+        hl.StatusLineNC = {
+          fg = "#8296B2",
+          bg = "#0D1728",
+        }
+
+        hl.TabLine = {
+          fg = "#5C6D84",
+          bg = "#080F1C",
+        }
+
+        hl.TabLineFill = {
+          bg = "#080F1C",
+        }
+
+        hl.TabLineSel = {
+          fg = "#C4D2E5",
+          bg = "#0B1220",
+          bold = true,
+        }
+
+        hl.WinBar = {
+          fg = "#8296B2",
+          bg = "#0B1220",
+        }
+
+        hl.WinBarNC = {
+          fg = "#5C6D84",
+          bg = "#0B1220",
+        }
+
+        hl.WinSeparator = {
+          fg = "#111D2E",
+          bg = c.bg,
+        }
+
+        hl.VertSplit = {
+          fg = "#111D2E",
+          bg = c.bg,
+        }
+
+        hl.Folded = {
+          fg = "#8296B2",
+          bg = "#101B2D",
+        }
+
+        hl.FoldColumn = {
+          fg = "#34445B",
+          bg = "#080F1C",
+        }
+      end,
+    },
+  },
+
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
